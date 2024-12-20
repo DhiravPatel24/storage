5fb2b09d710930ca122a949c24d09014f0f19e2b
DhiravPatel24 <160713525+DhiravPatel24@users.noreply.github.com>
2024-12-20 16:22:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "StoreIt",
+  title: "Storeit",
   description: "StoreIt - The only storage solution you need.",
 };
 
